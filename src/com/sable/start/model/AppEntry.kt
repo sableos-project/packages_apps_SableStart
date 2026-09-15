aab5d55e3d61bef6c89334d4c8aaea3828bc6f69
aimindseye <127505847+aimindseye@users.noreply.github.com>
2026-09-14 21:40:00 -0400

    R7: carry real icon and stable profile identity

--- src/com/sable/start/model/AppEntry.kt
+++ src/com/sable/start/model/AppEntry.kt
@@ -1,10 +1,16 @@
 package org.sableos.start.model
 
 import android.content.ComponentName
+import android.graphics.Bitmap
 import android.os.UserHandle
 
 data class AppEntry(
     val label: String,
     val component: ComponentName,
     val user: UserHandle,
-)
+    val profileSerial: Long,
+    val icon: Bitmap?,
+) {
+    val stableId: String
+        get() = "$profileSerial|${component.flattenToString()}"
+}
